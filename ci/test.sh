0b6fef3c4c40e52de507a2ab0606fd131a991105
Cheng Dian Yang <yangcd@cn.ibm.com>
2017-10-18 10:07:20 +0800

    m

--- ci/test.sh
+++ ci/test.sh
@@ -2,15 +2,15 @@
 set -e -x
 
 touch ssh_key
-echo $private_key
-echo $private_key > ssh_key
+echo "$private_key"
+echo "$private_key" > ssh_key
 cat ssh_key
 
 # install ssh
 apt-get update && apt-get -y install sudo
 apt-get -y install openssh-server tcl tk expect
 
-sudo ssh_key
+sudo chnod -R 600 ssh_key
 eval "$(ssh-agent -s)"
 ssh-add ssh_key
 ssh-add -l
