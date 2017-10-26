7444a82b0d106958d49c850468a877cfa05b0465
Cheng Dian Yang <yangcd@cn.ibm.com>
2017-10-26 15:33:52 +0800

    m

--- ci/test.sh
+++ ci/test.sh
@@ -1,23 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e -x
 
-touch ssh_key
-echo "$private_key"
-echo "$private_key" > ssh_key
-cat ssh_key
-
-# install ssh
-apt-get update && apt-get -y install sudo
-apt-get -y install openssh-server tcl tk expect
-
-sudo chnod -R 600 ssh_key
-eval "$(ssh-agent -s)"
-ssh-add ssh_key
-ssh-add -l
-
-expect << EOF
-spawn ssh -T git@github.ibm.com
-expect "Are you sure you want to continue connecting (yes/no)?"
-send "yes\r"
-expect eof
-EOF
+pushd flight-school
+  bundle install
+  bundle exec rspec
+popd
